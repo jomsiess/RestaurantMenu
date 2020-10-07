aeae635a3708b1d4aa6a4f58d583289028e768e2
jomsiess <jomsiess@gmail.com>
2020-10-06 21:04:47 -0500

    adding file

---
+++ RestaurantMenu/Menu.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace RestaurantMenu
+{
+    public class Menu
+    {
+        private List<MenuItem> items;
+        private DateTime pubdate;
+
+        public List<MenuItem> Items { get; set; }
+        public DateTime Pubdate { get; set; }
+
+        public Menu(List<MenuItem> items, DateTime pubdate)
+        {
+            this.items = items;
+            this.pubdate = pubdate; 
+        }
+
+        public Menu() : this(new List<MenuItem>(), DateTime.Now) { }
+    }
+}
